33bbb06a250bc115882c8afa1751e336b11f194c
WillC33 <william@williamcooke.net>
2024-02-02 10:50:35 +0000

    Initial commit

---
+++ WriterUI/Writer.cs
@@ -0,0 +1,77 @@
+﻿namespace WriterUI;
+
+/// <summary>
+/// Static class for calling writer methods on the console IO
+/// </summary>
+public static class Writer
+{
+    /// <summary>
+    /// Asks for input against a certain question or blank and returns the following input of type T
+    /// </summary>
+    /// <param name="request">the question</param>
+    /// <param name="customColour">An optional custom colour for valid input</param>
+    /// <returns>response string cast to T</returns>
+    public static T RequestInput<T>(string? request = null, ConsoleColor?  customColour = null)
+    {
+        Console.ForegroundColor = customColour ?? ConsoleColor.Cyan;
+        Console.WriteLine(request);
+        while (true)
+        {
+            var input = Console.ReadLine() ?? string.Empty;
+            try
+            {
+                T result = (T)Convert.ChangeType(input, typeof(T));
+                return result;
+            }
+            catch (FormatException)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Invalid input...");
+                Console.ForegroundColor = customColour ?? ConsoleColor.Cyan;
+            }
+            catch (OverflowException)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Input out of range...");
+                Console.ForegroundColor = customColour ?? ConsoleColor.Cyan;
+            }
+        }
+    }
+
+
+    /// <summary>
+    /// Updates the user with formatted messages
+    /// </summary>
+    /// <param name="message">the message to display</param>
+    /// <param name="customColour">An optional custom colour for the message</param>
+    public static void Message(string message, ConsoleColor?  customColour = null)
+    {
+        Console.ForegroundColor = customColour ?? ConsoleColor.White;
+        Console.WriteLine();
+        Console.WriteLine(message);
+    }
+    
+    
+    /// <summary>
+    /// Updates the user with formatted warning messages
+    /// </summary>
+    /// <param name="message">the message to display</param>
+    public static void Warn(string message)
+    {
+        Console.ForegroundColor = ConsoleColor.DarkYellow;
+        Console.WriteLine();
+        Console.WriteLine(message);
+    }
+    
+    
+    /// <summary>
+    /// Updates the user with formatted error messages
+    /// </summary>
+    /// <param name="message">the message to display</param>
+    public static void Error(string message)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine();
+        Console.WriteLine(message);
+    }
+}
---
+++ Writer_sandbox/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using WriterUI;
+
+Writer.Message("This is a trial");
+Writer.Error("And this is an error!");
+Writer.Warn("I'm warning you...");
+
+int intInput = Writer.RequestInput<int>("Please enter a number above 0");
+
+Console.WriteLine(intInput);
